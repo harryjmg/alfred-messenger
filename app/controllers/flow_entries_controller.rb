96a446274e76cd4cb478a152e51ca0d2ea3d59d3
Harry Gueguen <harry.gueguen@gmail.com>
2019-02-05 16:43:34 +0100

    Correct entries by entry

--- app/controllers/flow_entries_controller.rb
+++ app/controllers/flow_entries_controller.rb
@@ -1,6 +1,6 @@
 class FlowEntriesController < ApplicationController
 
 	def index
-		@flow_entries = User.find_by(psid: params[:id])&.flow_entries
+		@flow_entries = User.find_by(psid: params[:id])&.flow_entry
 	end
 end
