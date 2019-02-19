a2bc9ecb2eb703f2fa10194ea32b2a0d35770223
Harry Gueguen <harry.gueguen@gmail.com>
2019-02-19 19:30:53 +0100

    🔨 rewrite webhook action

--- app/controllers/typeform_controller.rb
+++ app/controllers/typeform_controller.rb
@@ -1,7 +1,15 @@
 class TypeformController < ApplicationController
 
   def webhook
-    puts "BAMBOU -= Receive something !"
-    puts params
+    begin
+      event = JSON.parse(request.body.read)
+      method = "handle_" + event['type'].tr('.', '_')
+      self.send method, event
+    rescue JSON::ParserError => e
+      render json: {:status => 400, :error => "Invalid payload"} and return
+    rescue NoMethodError => e
+      # missing event handler
+    end
+    render json: {:status => 200}
   end
 end
