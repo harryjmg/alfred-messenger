b6bc5c1e801d0a7b4cd7a92bc1c95e9c072a1b0b
Harry Gueguen <harry.gueguen@gmail.com>
2019-02-20 19:46:03 +0100

    ✨ Add welcome msg et d'autres details

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -30,18 +30,19 @@ Bot.on :message do |message|
 		the_user = User.new(psid: message.sender["id"])
 		the_user.private_id = SecureRandom.hex(4)
 		the_user.save
+		answer(message, "Bienvenue maggl. Alors le but ici c'est de relever ton état d'esprit au quotidien dans le but d'améliorer ta vie. Moi c'est Alfred et je suis principalement la pour te rappeler de répondre a un questionnaire a des intervalles aléatoires. Voila voila. Sinon les commandes que tu peux utiliser a partir de maintenant sont les suivantes [Start / Stop / Link / Help]. Des bisous")
 	else
 		if (message.text.upcase.include? "START")
 			the_user.update_attribute(:flow_testing, true)
-			answer(message, "Début du test")
+			answer(message, "Début du test. Bonne journée !")
 			the_user.start_flow_test
 		elsif (message.text.upcase.include? "STOP")
 			the_user.update_attribute(:flow_testing, false)
-			answer(message, "Fin du test pour aujourd'hui")
+			answer(message, "Fin du test pour aujourd'hui.")
 		elsif (message.text.upcase.include? "LINK")
-			answer(message, "Ton url : https://flowtracker03.herokuapp.com/flow_entries?id=#{the_user.private_id}")
+			answer(message, "Ton url : https://flowtracker03.herokuapp.com/flow_entries?id=#{the_user.private_id} . Si tu as des idées pour rendre plus pertinent l'affichage de tes résultats n'hésite pas a en parler a Harry (pas moi pcq je capte rien de ce que tu dis)")
 		elsif (message.text.upcase.include? "HELP")
-			answer(message, "Start / Stop / Link")
+			answer(message, "Start / Stop / Link . C'est pas sorcier")
 		else
 			answer(message, "Harry ne m'a pas appris ta langue dsl")
 		end
