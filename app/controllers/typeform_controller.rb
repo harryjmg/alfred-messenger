140d4ca577df26cb948af2392a0ddb74fca13e9a
Harry Gueguen <harry.gueguen@gmail.com>
2019-02-20 12:36:10 +0100

    🚧 Simplified to resolve bug

--- app/controllers/typeform_controller.rb
+++ app/controllers/typeform_controller.rb
@@ -1,31 +1,21 @@
 class TypeformController < ApplicationController
 
   def webhook
-    begin
-      puts "______ BONJOUR _______"
-      event = JSON.parse(request.body.read)
+    event = JSON.parse(request.body.read)
 
-      form_response = event["form_response"]
-      user_private_id = form_response["hidden"]["id"]
-      tested_user = User.find_by(private_id: user_private_id)
-      if tested_user.nil?
-        render json: {:status => 400, :error => "ID can't be found"} and return
-      end
-      answers = form_response["answers"]
-      flow_entry = FlowEntry.new(user: tested_user)
-      i = 0
-      answers.each do |answer|
-        choice = answer[answer["type"]]
-        flow_entry.update_attribute("#{FlowEntry::FLOW_ENTRY_ATTRIBUTES[i]}", choice)
-        i = i + 1
-      end
-      puts "______ AUREVOIR _______"
-
-    rescue JSON::ParserError => e
-      render json: {:status => 400, :error => "Invalid payload"} and return
-    rescue NoMethodError => e
-      # missing event handler
-      puts "______ SOMBRE ZONE _______"
+    form_response = event["form_response"]
+    user_private_id = form_response["hidden"]["id"]
+    tested_user = User.find_by(private_id: user_private_id)
+    if tested_user.nil?
+      render json: {:status => 400, :error => "ID can't be found"} and return
+    end
+    answers = form_response["answers"]
+    flow_entry = FlowEntry.new(user: tested_user)
+    i = 0
+    answers.each do |answer|
+      choice = answer[answer["type"]]
+      flow_entry.update_attribute("#{FlowEntry::FLOW_ENTRY_ATTRIBUTES[i]}", choice)
+      i = i + 1
     end
 
     render json: {:status => 200}
