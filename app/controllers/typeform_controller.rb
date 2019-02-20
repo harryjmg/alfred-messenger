62d146add3233bc97fa3e256dc7d5dd7ec433a34
Harry Gueguen <harry.gueguen@gmail.com>
2019-02-20 12:06:04 +0100

    ⚡️ Should save typeform result (first try)

--- app/controllers/typeform_controller.rb
+++ app/controllers/typeform_controller.rb
@@ -3,15 +3,29 @@ class TypeformController < ApplicationController
   def webhook
     begin
       event = JSON.parse(request.body.read)
-      puts "____________ BONJOUR ____________________"
-      puts event
-      method = "handle_" + event['type'].tr('.', '_')
-      self.send method, event
+
+      form_response = event["form_response"]
+      user_private_id = form_response["hidden"]["id"]
+      tested_user = User.find_by(private_id: user_private_id)
+      if tested_user.nil?
+        render json: {:status => 400, :error => "ID can't be found"} and return
+      end
+      answers = form_response["answers"]
+      flow_entry = FlowEntry.new(user: tested_user)
+      i = 0
+      answers.each do |answer|
+        choice = answer[answer["type"]]
+        flow_entry.write_attribute("#{FlowEntry::FLOW_ENTRY_ATTRIBUTES[i]}", choice)
+        i = i + 1
+      end
+      flow_entry.save
+
     rescue JSON::ParserError => e
       render json: {:status => 400, :error => "Invalid payload"} and return
     rescue NoMethodError => e
       # missing event handler
     end
+
     render json: {:status => 200}
   end
 end
--- app/models/flow_entry.rb
+++ app/models/flow_entry.rb
@@ -1,5 +1,21 @@
 class FlowEntry < ApplicationRecord
 	belongs_to :user
 
-	
+	FLOW_ENTRY_ATTRIBUTES = [
+		"activity_name",
+		"activity_categorty",
+  	"challenge_level",
+  	"skill_level",
+  	"feeling_competent",
+  	"mentally_absorbed",
+  	"clear_objectives",
+  	"control_situation",
+  	"free_from_ego",
+  	"free_from_time",
+  	"clear_conditions",
+  	"great_experience",
+  	"is_voluntary",
+  	"happiness_level",
+  	"social_quality"
+	]
 end
