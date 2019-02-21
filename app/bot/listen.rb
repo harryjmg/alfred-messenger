704839f6084bad33e319848a5d0333a6a2995bca
Harry Gueguen <harry.gueguen@gmail.com>
2019-02-21 21:53:30 +0100

    ✨ Add message when trying to access when no data or no user

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -45,16 +45,3 @@ Bot.on :message do |message|
 		end
 	end
 end
-
-Bot.on :postback do |postback|
-	postback.sender    # => { 'id' => '1008372609250235' }
-	postback.recipient # => { 'id' => '2015573629214912' }
-	postback.sent_at   # => 2016-04-22 21:30:36 +0200
-	postback.payload   # => 'EXTERMINATE'
-
-	if postback.payload == 'EXTERMINATE'
-		puts "Human #{postback.recipient} marked for extermination"
-	else
-		puts "Coco"
-	end
-end
--- app/controllers/flow_entries_controller.rb
+++ app/controllers/flow_entries_controller.rb
@@ -1,6 +1,16 @@
 class FlowEntriesController < ApplicationController
 
 	def index
-		@flow_entries = User.find_by(private_id: params[:id])&.flow_entry.sorted_by_date
+
+		@flow_entries = User.find_by(private_id: params[:id])&.flow_entry&.sorted_by_date
+
+		if @flow_entries.nil?
+			@flow_entries_error = [ true, "Utilisateur non trouvé. As tu utilisé le bon lien ?" ]
+		elsif @flow_entries.count = 0
+			@flow_entries_error = [ true, "Pas encore de données, lance le test et passe ta journée normalement" ]
+		else
+			@flow_entries_error = [ false, "Ok" ]
+		end
+
 	end
 end
