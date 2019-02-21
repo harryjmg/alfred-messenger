9879070241b5457a63c3d3a99c210e03c9314992
Harry Gueguen <harry.gueguen@gmail.com>
2019-02-21 16:47:56 +0100

    ✨ Now table have colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,44 @@
 module ApplicationHelper
-end
+  def bg_for_feelings level
+    case level
+    when 0
+      "#F78181"
+    when 1
+      "#F7BE81"
+    when 2
+      "#D8F781"
+    when 3
+      "#9FF781"
+    when 4
+      "#58ACFA"
+    else
+      "white"
+    end
+  end
+
+  def bg_for_state state
+    case state
+    when "Flow"
+      "#81DAF5"
+    when "Stimulé"
+      "#58ACFA"
+    when "Controle"
+      "#CEECF5"
+    when "Relaxation"
+      "#04B404"
+    when "Ennui"
+      "#CEF6CE"
+    when "Apathie"
+      "#D8D8D8"
+    when "Inquiétude"
+      "#A4A4A4"
+    when "Anxiété"
+      "#585858"
+    else
+      "white"
+    end
+  end
+
+  def bg_for_bool bool
+    bool? "#58ACFA" : "#F78181"
+  end
