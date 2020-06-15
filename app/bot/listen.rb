bed7d0e48c212fec08c1444512c41dacbc25ae41
Harry Gueguen <harry.gueguen@gmail.com>
2020-06-15 18:57:04 +0200

    🐞 Fix the unavailable account problem

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -22,42 +22,45 @@ def send_to_all_recent_users text
 end
 
 Bot.on :message do |message|
-	return if message.nil?
-	puts "Sender psid = #{message.sender}"
+	begin
+		puts "Sender psid = #{message.sender}"
 
-	# Set "vu" and "typing..."
-	message.mark_seen
-	message.typing_on
+		# Set "vu" and "typing..."
+		message.mark_seen
+		message.typing_on
 
-	# Handle the user
-	the_user = User.find_by(psid: message.sender["id"])
-	if the_user.nil?
-		the_user = User.new(psid: message.sender["id"])
-		the_user.private_id = SecureRandom.hex(4)
-		the_user.save
+		# Handle the user
+		the_user = User.find_by(psid: message.sender["id"])
+		if the_user.nil?
+			the_user = User.new(psid: message.sender["id"])
+			the_user.private_id = SecureRandom.hex(4)
+			the_user.save
+			message.typing_off
+			answer(message, "Première utilisation ? J'ai créé une petite notice pour les nouveaux : https://flowtracker03.herokuapp.com/about")
+		end
+
+		# Handle the message
 		message.typing_off
-		answer(message, "Première utilisation ? J'ai créé une petite notice pour les nouveaux : https://flowtracker03.herokuapp.com/about")
-	end
-
-	# Handle the message
-	message.typing_off
-	if (message.text&.upcase&.include? "START")
-		the_user.update_attribute(:flow_testing, true)
-		answer(message, "Début du test. Bonne journée !")
-		the_user.start_flow_test
-	elsif (message.text&.upcase&.include? "STOP")
-		the_user.update_attribute(:flow_testing, false)
-		answer(message, "Fin du test pour aujourd'hui")
-	elsif (message.text&.upcase&.include? "LINK")
-		answer(message, "Ton url : https://flowtracker03.herokuapp.com/flow_entries?id=#{the_user.private_id} . Si tu as des idées pour rendre plus pertinent l'affichage de tes résultats n'hésite pas a en parler a Harry (pas moi pcq je capte rien de ce que tu dis)")
-	elsif (message.text&.upcase&.include? "HELP")
-		answer(message, "Start / Stop / Link . C'est pas sorcier")
-	# elsif ((message.text&.upcase&.include? "MESSAGE HARRY") && the_user.role == "admin")
-	# 	# Broadcast a message to all recent users
-	# 	n = send_to_all_recent_users(message.text)
-	# 	answer(message, "Broadcast launched")
-	# 	answer(message, "Successfully sent to #{n} recent users (total users : #{User.all.count})")
-	else
-		answer(message, "Harry ne m'a pas appris ta langue dsl")
+		if (message.text&.upcase&.include? "START")
+			the_user.update_attribute(:flow_testing, true)
+			answer(message, "Début du test. Bonne journée !")
+			the_user.start_flow_test
+		elsif (message.text&.upcase&.include? "STOP")
+			the_user.update_attribute(:flow_testing, false)
+			answer(message, "Fin du test pour aujourd'hui")
+		elsif (message.text&.upcase&.include? "LINK")
+			answer(message, "Ton url : https://flowtracker03.herokuapp.com/flow_entries?id=#{the_user.private_id} . Si tu as des idées pour rendre plus pertinent l'affichage de tes résultats n'hésite pas a en parler a Harry (pas moi pcq je capte rien de ce que tu dis)")
+		elsif (message.text&.upcase&.include? "HELP")
+			answer(message, "Start / Stop / Link . C'est pas sorcier")
+		# elsif ((message.text&.upcase&.include? "MESSAGE HARRY") && the_user.role == "admin")
+		# 	# Broadcast a message to all recent users
+		# 	n = send_to_all_recent_users(message.text)
+		# 	answer(message, "Broadcast launched")
+		# 	answer(message, "Successfully sent to #{n} recent users (total users : #{User.all.count})")
+		else
+			answer(message, "Harry ne m'a pas appris ta langue dsl")
+		end
+	rescue Facebook::Messenger::FacebookError => e
+		logger.debug(e.inspect)
 	end
 end
